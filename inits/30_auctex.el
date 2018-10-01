594a5a29885bdb9e4d02edd7ac6016beef321c1c
Shinsuke OTA <ota@cns.s.u-tokyo.ac.jp>
2018-10-02 00:09:42 +0900

    [mod] reftex

--- inits/30_auctex.el
+++ inits/30_auctex.el
@@ -19,10 +19,12 @@
 ;; find file in the present directory
 (setq LaTeX-includegraphics-read-file 'LaTeX-includegraphics-read-file-relative)
 
+
 (add-hook 'LaTeX-mode-hook
   (lambda ()
     (setq TeX-command-extra-options "-shell-escape")
   ))
+
 
 
 (add-hook 'LaTeX-mode-hook
@@ -31,3 +33,14 @@
                           '("LatexMK-pLaTeX"
                             "latexmk -e '$latex=q/platex %%O %(file-line-error) %(extraopts) %S %(mode) %%S/' -e '$bibtex=q/pbibtex %%O %%B/' -e '$biber=q/biber %%O --bblencoding=utf8 -u -U --output_safechars %%B/' -e '$makeindex=q/upmendex %%O -o %%D %%S/' -e '$dvipdf=q/dvipdfmx %%O -o %%D %%S/' -norc -gg -pdfdvi %t"
                             TeX-run-TeX nil (latex-mode) :help "Run LatexMK-pLaTeX"))))
+
+(setq reftex-bibpath-environment-variables
+      '("~/Work/Bibtex"))
+(setq reftex-use-external-file-finders t)
+(setq reftex-external-file-finders
+       '(("tex" . "kpsewhich -format=.tex %f")
+        ("bib" . "kpsewhich -format=.bib %f")))
+
+(setq reftex-bibliography-commands '("bibliography" "nobibliography" "addbibresource"))
+
+
